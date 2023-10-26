ddfa54fa3b92ea0838043eec1120405c9da086bb
juan-angel <juan.angel.gascon@gmail.com>
2023-10-26 15:53:19 +0200

    Modules and project created

---
+++ java8/src/main/java/com/ing/jangel/containerawareness/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.ing.jangel.containerawareness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ java8/src/test/java/com/ing/jangel/containerawareness/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ing.jangel.containerawareness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
